41e86263ba7d7b5fd2d7895b55303da4a03dbf30
BERGAMMOTIK <59376854+BERGam-grud@users.noreply.github.com>
2026-07-07 12:18:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ from routers import auth, posts, measurements, alarms, users, thresholds, sync, 
 from routers import ws
 from core.notifications import notification_worker
 from core.migrations import run_migrations
+from routers import sync_v2
 
 app = FastAPI(
     title="РМС — Центральний сервер",
@@ -30,7 +31,7 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
+app.include_router(sync_v2.router)
 app.include_router(auth.router)
 app.include_router(posts.router)
 app.include_router(measurements.router)
